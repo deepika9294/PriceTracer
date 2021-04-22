5f1d0d5bdcce31a416beba006b04cdc02105ac85
Deepika Goyal <deepika.goyal0803@gmail.com>
2021-04-22 13:52:03 +0530

    major fix: while verification of user

--- src/Component/Land.js
+++ src/Component/Land.js
@@ -4,8 +4,8 @@ export class Land extends Component {
     render() {
         return (
             <div>
-                <h2 class="intro1">Welcome to <br></br> Price Tracer !!</h2>
-                <h2 class="intro">Track Prices of Your Favourite Products..</h2>
+                <h2 className="intro1">Welcome to <br></br> Price Tracer !!</h2>
+                <h2 className="intro">Track Prices of Your Favourite Products..</h2>
             </div>
         )
     }
