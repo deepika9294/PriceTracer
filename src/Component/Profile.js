3ea1a74cf11970a950c268c4760c90d9a34b8819
Deepika Goyal <deepika.goyal0803@gmail.com>
2021-04-22 17:20:25 +0530

    add: Profile page

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import {BACKEND} from '../config';
+
 
 export default class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            user : {},
+            reload : false,
+        }
+    }
+    
+
+
+    componentDidMount(){
+        (async () =>{
+            const id = localStorage.getItem('id');
+           await axios
+            .get( `${BACKEND}/users/getuser/${id}`)
+            .then(res => {
+                console.log(res.data)
+               
+                this.setState({
+                    user : res.data,
+                    reload: true
+                })
+                // console.log("user" )
+                // console.log(this.state.user);
+            })
+            .catch(err => console.log("failed to fetch products"));
+            
+        })();  
+    }
+
+
+
+
     render() {
-        return (
-            <div>
-                hi
+        return this.state.reload? (
+            <div className="auth-wrapper">
+            <div className="auth-inner">
+                <h2>Profile</h2>
+                <p>Name: {this.state.user.name} </p>
+                <p>Email: {this.state.user.email}</p>
+                <p>Mobile No: {this.state.user.contactNo}</p>
+                <p>Mode: {this.state.user.mode}</p>
+                <p>Products Count:{this.state.user.Cart.length}</p>
+
+                </div>
+
             </div>
-        )
+        ): (<div>
+            Loading
+        </div>)
     }
 }
